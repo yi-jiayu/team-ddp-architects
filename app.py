28d901aee17ba61063a45b44d17f11d099809372
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2017-09-23 13:49:22 +0800

    Add logging line

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def hello_world():
 @app.route('/trainPlanner', methods=['POST'])
 def train_planner_endpoint():
     json_str = request.get_json()
+    print(json_str)
     destination, stations = train_planner.parse_input(json_str)
     line, num_passengers, station = train_planner.plan(destination, stations)
     answer = {'line': line, 'totalNumOfPassengers': num_passengers, 'reachingVia': station}
