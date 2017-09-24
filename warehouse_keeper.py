d24e3534aa6cb507d8d25169c57984ad371dc06c
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2017-09-24 09:06:26 +0800

    Update Warehouse Keeper.py

--- warehouse_keeper.py
+++ warehouse_keeper.py
@@ -2,6 +2,7 @@ from queue import PriorityQueue
 from copy import deepcopy
 from collections import deque
 import requests
+from munkres import Munkres
 
 
 def clean_map(map_):
@@ -29,23 +30,98 @@ def find_boxes(map_):
                 yield (r, c)
 
 
-def adjacent(map_, loc):
+def adjacent(map_, loc, walls=('x',)):
     r, c = loc
+
+    # up open
     if r > 0:
-        if map_[r - 1][c] != 'x':
+        if map_[r - 1][c] not in walls:
             yield (r - 1, c)
 
-    if r < len(map_) - 1:
-        if map_[r + 1][c] != 'x':
-            yield (r + 1, c)
-
+    # left open
     if c > 0:
-        if map_[r][c - 1] != 'x':
+        if map_[r][c - 1] not in walls:
             yield (r, c - 1)
 
+    # down open
+    if r < len(map_) - 1:
+        if map_[r + 1][c] not in walls:
+            yield (r + 1, c)
+
+    # right open
     if c < len(map_[0]) - 1:
-        if map_[r][c + 1] != 'x':
+        if map_[r][c + 1] not in walls:
             yield (r, c + 1)
+
+
+def accessible_player_positions(state):
+    pos = find_keeper(state)
+    q = deque((pos,))
+    visited = set()
+
+    while q:
+        curr = q.pop()
+        visited.add(curr)
+
+        for adj in adjacent(state, curr, ('x', 'o')):
+            if adj not in visited:
+                q.appendleft(adj)
+
+    return visited
+
+
+def opposite(origin, beside):
+    r1, c1 = origin
+    r2, c2 = beside
+
+    if r1 == r2:
+        if c1 > c2:
+            return r1, c1 - 1
+        else:
+            return r1, c1 + 1
+    else:
+        if r1 > r2:
+            return r1 + 1, c1
+        else:
+            return r1 - 1, c1
+
+
+def possible_pushes(state, accessible_locations):
+    for r, c in find_boxes(state):
+        # push down
+        if r > 0 and state[r-1][c] in accessible_locations and state[r+1][c] != 'x':
+            next_state = deepcopy(state)
+            next_state[r][c] = 'b'
+            next_state[r+1][c] = 'o'
+            yield 'down', next_state
+
+        # push up
+        if r < len(state) - 1 and state[r+1][c] in accessible_locations and state[r-1][c] != 'x':
+            next_state = deepcopy(state)
+            next_state[r][c] = 'b'
+            next_state[r - 1][c] = 'o'
+            yield 'up', next_state
+
+        # push right
+        if c > 0 and state[r][c-1] in accessible_locations and state[r][c+1] != 'x':
+            next_state = deepcopy(state)
+            next_state[r][c] = 'b'
+            next_state[r][c+1] = 'o'
+            yield 'right', next_state
+
+        # push left
+        if c < len(state[0]) - 1 and state[r][c+1] in accessible_locations and state[r][c-1] != 'x':
+            next_state = deepcopy(state)
+            next_state[r][c] = 'b'
+            next_state[r][c-1] = 'o'
+            yield 'left', next_state
+
+
+def normalised_player_position(accessible_locations):
+    max_r = max(p[0] for p in accessible_locations)
+    max_c = max(p[1] for p in accessible_locations if p[0] == max_r)
+
+    return max_r, max_c
 
 
 def distances_from_target(map_, origin):
@@ -75,8 +151,12 @@ def distances_from_nearest_target(map_):
     return combined_distances
 
 
-def heuristic(map_, distances):
-    return sum(distances[box] for box in find_boxes(map_))
+def heuristic(map_, depth_maps):
+    boxes = list(find_boxes(map_))
+    cost_matrix = [[dm[b] for dm in depth_maps] for b in boxes]
+    m = Munkres()
+    indexes = m.compute(cost_matrix)
+    return sum(cost_matrix[r][c] for r, c in indexes)
 
 
 def get_possible_moves(map_):
@@ -146,41 +226,69 @@ def get_possible_moves(map_):
 #     print('\n')
 
 
-def freeze_map(map_):
-    s = ''
-    for row in map_:
-        s += ''.join(row)
-    return s
+def check_for_dead_positions(state, stars):
+    """Returns true if any stone is in a dead position which is not a target."""
+
+    stones = find_boxes(state)
+    for r, c in stones:
+        if (r, c) not in stars:
+            adj = list(adjacent(state, (r, c)))
+            if len(adj) == 1:
+                return True
+            elif len(adj) == 2 and adj[0][0] != adj[1][0] and adj[0][1] != adj[1][1]:
+                for row in state:
+                    print(row)
+                return True
+    return False
+
+
+def freeze_map(state, accessible_locations):
+    """States are considered equivalent when all the stone positions and the top-right accessible location by the
+    player is the same. """
+    s = frozenset(find_boxes(state))
+    return normalised_player_position(accessible_locations), s
 
 
 def solve(map_):
-    distances = distances_from_nearest_target(map_)
-    stars = list(find_targets(map_))
+    targets = find_targets(map_)
+    depth_maps = [distances_from_target(map_, t) for t in targets]
+
     map_ = clean_map(map_)
+    h_0 = heuristic(map_, depth_maps)
 
-    q = PriorityQueue()
-    h_0 = heuristic(map_, distances)
-    q.put((h_0, 0, h_0, map_, ()))
-    visited = set()
+    max_depth = h_0
+    while True:
+        q = PriorityQueue()
+        q.put((h_0, 0, h_0, map_, ()))
+        visited = set()
 
-    while not q.empty():
-        f, g, h, state, actions = q.get()
+        while not q.empty():
+            f, g, h, state, actions = q.get()
 
-        frozen = freeze_map(state)
-        visited.add(frozen)
+            accessible_locations = accessible_player_positions(state)
+            frozen = freeze_map(accessible_locations)
+            visited.add(frozen)
 
-        if h == 0:
-            return actions
+            if h == 0:
+                return actions
 
-        for direction, next_state in get_possible_moves(state):
-            if freeze_map(next_state) not in visited:
-                g_1 = g + 1
-                h_1 = heuristic(next_state, distances)
-                f_1 = g_1 + h_1
-                new_actions = actions + (direction,)
-                q.put((f_1, g_1, h_1, next_state, new_actions))
+            for direction, next_state in possible_pushes(state, accessible_locations):
+                if freeze_map(next_state) not in visited:
+                    g_1 = g + 1
 
-    return None
+                    # ids
+                    if g_1 > max_depth:
+                        continue
+
+                    # if check_for_dead_positions(next_state, targets):
+                    #     continue
+
+                    h_1 = heuristic(next_state, depth_maps)
+                    f_1 = g_1 + h_1
+                    new_actions = actions + (direction,)
+                    q.put((f_1, g_1, h_1, next_state, new_actions))
+
+        max_depth += 1
 
 
 def solve_one_map(run_id, solution):
@@ -206,7 +314,7 @@ def solve_async(run_id, first_map):
 
 
 if __name__ == '__main__':
-    map_ = ["xx---xxx",
+    map1 = ["xx---xxx",
             "xx-*-xxx",
             "xx- ----",
             "---o o*-",
@@ -215,16 +323,35 @@ if __name__ == '__main__':
             "xxx-*-xx",
             "xxx---xx"]
 
-    print(solve(map_))
+    print(solve(map1))
 
-    map2 = ['-----xxxx',
-            '-b  -xxxx',
-            '- oo- ---',
-            '- o - -*-',
-            '--- ---*-',
-            'x--    *-',
-            'x-   -  -',
-            'x-   ----',
-            'x-----xxx']
+    # map1 = clean_map(map1)
+    # print(normalised_player_position(map1))
 
-    print(solve(map2))
+    # map2 = ['-----xxxx',
+    #         '-b  -xxxx',
+    #         '- oo- ---',
+    #         '- o - -*-',
+    #         '--- ---*-',
+    #         'x--    *-',
+    #         'x-   -  -',
+    #         'x-   ----',
+    #         'x-----xxx']
+    #
+    # print(solve(map2))
+    #
+    # map3 = ['----------',
+    #         '-**      -',
+    #         '-**o  -  -',
+    #         '-  -o-- --',
+    #         '- o     -x',
+    #         '----- - -x',
+    #         'xx- o b -x',
+    #         'xx-     -x',
+    #         'xx-------x']
+    #
+    # print(solve(map3))
+
+    # targets = find_targets(map3)
+    # depth_maps = [distances_from_target(map3, t) for t in targets]
+    # print(heuristic(map3, depth_maps))
